df455572da8a10c3f0dfede77a939faea2f0031f
Stefan Hanke <Stefan.Hanke@external.telekom.de>
2016-02-12 07:44:37 +0100

    add first three.js examples

---
+++ app/js/geometry.js
@@ -0,0 +1,52 @@
+function updateGroupGeometry(mesh, geometry) {
+
+	mesh.children[0].geometry.dispose();
+	mesh.children[1].geometry.dispose();
+
+	mesh.children[0].geometry = new THREE.WireframeGeometry(geometry);
+	mesh.children[1].geometry = geometry;
+
+}
+
+function chooseFromHash(mesh) {
+
+	var data = {
+		text : "PP",
+		size : 20,
+		height : 5,
+		curveSegments : 20,
+		font : "optimer",
+		weight : "bold",
+		bevelEnabled : true,
+		bevelThickness : 1.5,
+		bevelSize : 1
+	};
+
+	function generateGeometry() {
+
+		var loader = new THREE.FontLoader();
+		loader.load('/js/' + data.font + '_' + data.weight + '.typeface.js', function(font) {
+
+			var geometry = new THREE.TextGeometry(data.text, {
+				font : font,
+				size : data.size,
+				height : data.height,
+				curveSegments : data.curveSegments,
+				bevelEnabled : data.bevelEnabled,
+				bevelThickness : data.bevelThickness,
+				bevelSize : data.bevelSize
+			});
+			geometry.center();
+
+			updateGroupGeometry(mesh, geometry);
+
+		});
+
+	}
+
+	//Hide the wireframe
+	mesh.children[0].visible = false;
+
+	generateGeometry();
+
+}
